be2a505dc6b0905fe860d5507b0689eb47718ff9
jimmyc2006 <shuwei@iqianjin.com>
2020-04-24 09:54:13 +0800

    背包问题1

---
+++ src/main/java/shuwei/improve/bag/Bag1.java
@@ -0,0 +1,57 @@
+package shuwei.improve.bag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shuwei
+ * @version 创建时间：2020年4月24日 上午8:31:45 类说明
+ */
+// 有N件物品和一个容量为V的背包。第i件物品的费用是c[i]，价值是w[i]。
+// 求解将哪些物品装入背包可使这些物品的费用总和不超过背包容量，且价值总和最大。
+// 状态转移方程是： f[i][v]=max{f[i-1][v],f[i-1][v-c[i]]+w[i]}
+public class Bag1 {
+
+  public Map<Integer, int[]> init() {
+    Map<Integer, int[]> data = new HashMap<>();
+    data.put(1, new int[] {2, 3});
+    data.put(2, new int[] {3, 4});
+    data.put(3, new int[] {4, 5});
+    data.put(4, new int[] {5, 8});
+    data.put(5, new int[] {9, 10});
+    return data;
+  }
+
+  public int bagPro(Map<Integer, int[]> data, int maxWeight) {
+    int[][] result = new int[data.size() + 1][maxWeight + 1];
+    for (int i = 1; i <= data.size(); i++) {
+      for (int j = 1; j <= maxWeight; j++) {
+        if (j - data.get(i)[0] >= 0) {
+          result[i][j] =
+              Math.max(result[i - 1][j], result[i - 1][j - data.get(i)[0]] + data.get(i)[1]);
+        } else {
+          result[i][j] = result[i - 1][j];
+        }
+      }
+    }
+    return result[data.size()][maxWeight];
+  }
+
+  // 递归实现方式
+  public int bagProRecursion(Map<Integer, int[]> data, int maxKey, int maxWeight) {
+    if (maxKey <= 0) {
+      return 0;
+    }
+    if (maxWeight < data.get(1)[0]) {
+      return 0;
+    }
+    int maxResult = 0;
+    if (maxWeight - data.get(maxKey)[0] >= 0) {
+      maxResult = Math.max(bagProRecursion(data, maxKey - 1, maxWeight),
+          bagProRecursion(data, maxKey - 1, maxWeight - data.get(maxKey)[0]) + data.get(maxKey)[1]);
+    } else {
+      maxResult = bagProRecursion(data, maxKey - 1, maxWeight);
+    }
+    return maxResult;
+  }
+}
---
+++ src/test/java/shuwei/improve/bag/Bag1Test.java
@@ -0,0 +1,23 @@
+package shuwei.improve.bag;
+/** 
+* @author shuwei 
+* @version 创建时间：2020年4月24日 上午8:56:04 
+* 类说明 
+*/
+
+import java.util.Map;
+
+import org.junit.Test;
+
+
+public class Bag1Test {
+  
+  @Test
+  public void test() {
+    Bag1 b = new Bag1();
+    Map<Integer, int[]> init = b.init();
+    int max = b.bagProRecursion(b.init(), init.size(), 20);
+    System.out.println(max);
+    System.out.println(b.bagPro(init, 20));
+  }
+}
