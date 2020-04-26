aa364e8cd5ac9247524cea7bd43002c904b5181d
jimmyc2006 <shuwei@iqianjin.com>
2020-04-26 19:58:42 +0800

    tmp

--- src/main/java/shuwei/leetcode/cn/four/six/Solution.java
+++ src/main/java/shuwei/leetcode/cn/four/six/Solution.java
@@ -1,23 +1,39 @@
 package shuwei.leetcode.cn.four.six;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-/** 
-* @author shuwei 
-* @version 创建时间：2020年4月25日 下午8:37:37 
-* 类说明 
-*/
 public class Solution {
+  
   public List<List<Integer>> permute(int[] nums) {
-    List<List<Integer>> result = new ArrayList<>();
+    List<List<Integer>> finalResult = new ArrayList<>();
+    Queue<Integer> q = new LinkedList<>();
     for (int i = 0; i < nums.length; i++) {
-      
+      q.add(nums[i]);
     }
+    int[] result = new int[nums.length];
+    selectAll(q, result, finalResult);
+    return finalResult;
   }
   
-  // 从startIndex开始选择
-  public int select(int[] nums, int startIndex) {
-    
+  private void selectAll(Queue<Integer> q, int[] result, List<List<Integer>> finalResult) {
+    int size = q.size();
+    if (size == 1) {
+      result[result.length - size] = q.peek();
+      List<Integer> list = new ArrayList<>(result.length);
+      for (int i = 0; i < result.length; i++) {
+        list.add(i, result[i]);
+      }
+      finalResult.add(list);
+      return;
+    }
+    for (int i = 0; i < size; i++) {
+      int tmp = q.poll();
+      result[result.length - size] = tmp;
+      selectAll(q, result, finalResult);
+      q.add(tmp);
+    }
   }
 }
