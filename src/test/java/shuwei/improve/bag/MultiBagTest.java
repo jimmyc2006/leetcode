b862b7c19aa91ed3fe4aaab03d3ca2c92724925c
jimmyc2006 <shuwei@163.com>
2020-06-22 00:17:42 +0800

    ¶àÖر³°üºͶþ½øÖÆÓŻ¯

---
+++ src/test/java/shuwei/improve/bag/MultiBagTest.java
@@ -0,0 +1,11 @@
+package shuwei.improve.bag;
+
+import org.junit.Test;
+
+public class MultiBagTest {
+  @Test
+  public void test() {
+    MultiBag mb = new MultiBag();
+    System.out.print(mb.myMax(5));
+  }
+}
