47297450b45c10a5c8d9ea346a7273cf67ab6c34
jimmyc2006 <shuwei@163.com>
2020-06-24 06:54:46 +0800

    jj

--- src/main/java/shuwei/leetcode/cn/one/zero/Solution.java
+++ src/main/java/shuwei/leetcode/cn/one/zero/Solution.java
@@ -1,13 +1,47 @@
 package shuwei.leetcode.cn.one.zero;
 
 public class Solution {
+
+  // dp解法
   public boolean isMatch(String s, String p) {
+    boolean[][] f = new boolean[s.length() + 1][p.length() + 1];
+    f[0][0] = true;
+    for (int i = 0; i <= s.length(); i++) {
+      for (int j = 1; j <= p.length(); j++) {
+        // j = 0表示空串，那么j = 1表示字符串的第一个
+        if (p.charAt(j - 1) == '*') {
+          f[i][j] = f[i][j - 2];
+          if (isM(s, p, i, j - 1)) {
+            f[i][j] = f[i - 1][j] || f[i][j - 2];
+          }
+        } else {
+          if (isM(s, p, i, j)) {
+            f[i][j] = f[i - 1][j - 1];
+          } else {
+            f[i][j] = false;
+          }
+        }
+      }
+    }
+    return f[s.length()][p.length()];
+  }
+
+  private boolean isM(String s, String p, int i, int j) {
+    if (i == 0) {
+      return false;
+    }
+    return (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
+  }
+
+
+  public boolean isMatch1(String s, String p) {
     if (s == null || p == null) {
       return false;
     }
     return match(s.toCharArray(), 0, p.toCharArray(), 0);
   }
- // "ab", ".*c"
+
+  // "ab", ".*c"
   private boolean match(char[] content, int i, char[] pattern, int j) {
     if (i >= content.length) {
       if (j >= pattern.length) {
@@ -31,7 +65,8 @@ public class Solution {
       return false;
     }
   }
- // "ab", ".*c"
+
+  // "ab", ".*c"
   private boolean mat(char[] content, int i, char[] pattern, int j, char pre) {
     if (j >= pattern.length) {
       return matTilEnd(content, i, pre);
--- src/test/java/shuwei/leetcode/cn/one/zero/SolutionTest.java
+++ src/test/java/shuwei/leetcode/cn/one/zero/SolutionTest.java
@@ -9,16 +9,16 @@ public class SolutionTest {
   public void test() {
     Solution s = new Solution();
     Assert.assertEquals(false, s.isMatch("ab", ".*c"));
-    Assert.assertEquals(true, s.isMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*"));
-    Assert.assertEquals(true, s.isMatch("aaa", "ab*a*c*a"));
     Assert.assertEquals(false, s.isMatch("aa", "a"));
     Assert.assertEquals(true, s.isMatch("aa", "a*"));
     Assert.assertEquals(true, s.isMatch("ab", ".*"));
-    Assert.assertEquals(false, s.isMatch("mississippi", "mis*is*p*."));
     Assert.assertEquals(true, s.isMatch("", "a*"));
     Assert.assertEquals(true, s.isMatch("aab", "c*a*b"));
     Assert.assertEquals(false, s.isMatch("aaa", "aaaa"));
     Assert.assertEquals(true, s.isMatch("aaa", "a*a"));
+    Assert.assertEquals(true, s.isMatch("aaa", "ab*a*c*a"));
     Assert.assertEquals(true, s.isMatch("ssissippi", "s*is*ip*."));
+    Assert.assertEquals(false, s.isMatch("mississippi", "mis*is*p*."));
+    Assert.assertEquals(true, s.isMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*"));
   }
 }
