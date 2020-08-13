1edd6cbbb3782acd22e34254c7b15420bbbfeb71
shuwei17 <shuwei17@360buyad.local>
2020-08-13 14:50:03 +0800

    day

---
+++ src/test/java/shuwei/leetcode/cn/four/three/SolutionTest.java
@@ -0,0 +1,14 @@
+package shuwei.leetcode.cn.four.three;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        System.out.println(s.multiply("789", "0"));
+        System.out.println(s.multiply("789", "222"));
+    }
+
+}
