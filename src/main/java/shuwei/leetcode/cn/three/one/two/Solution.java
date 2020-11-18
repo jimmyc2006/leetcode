fda07c93577966b492c63c41a8048d9f9046cd62
jimmyc2006 <shuwei@163.com>
2020-11-18 23:23:40 +0800

    repeat 312 chuoqiqiu

--- src/main/java/shuwei/leetcode/cn/three/one/two/Solution.java
+++ src/main/java/shuwei/leetcode/cn/three/one/two/Solution.java
@@ -1,6 +1,36 @@
 package shuwei.leetcode.cn.three.one.two;
 
+import java.util.Arrays;
+
 public class Solution {
+  // 复习2020-11-18
+  public int maxCoins(int[] nums) {
+    int length = nums.length;
+    // dp[i][j][k] 表示戳破i和k之间的j可得的分数
+    // dp[i][k]之间的最大值是
+    int[] data = new int[length + 2];
+    data[0] = 1;
+    System.arraycopy(nums, 0, data, 1, length);
+    data[length + 1] = 1;
+    int[][] dp = new int[data.length][data.length];
+    deal(0, data.length - 1, dp, data);
+    return dp[0][data.length - 1];
+  }
+  private int deal(int start, int end, int[][] dp, int[] data) {
+    if (start >= end - 1) {
+      return 0;
+    }
+    if (dp[start][end] > 0) {
+      return dp[start][end];
+    }
+    int max = 0;
+    for (int i = start + 1; i < end; i++) {
+      max = Math.max(max, deal(start, i, dp, data) + deal(i, end, dp, data) + data[i] * data[start] * data[end]);
+    }
+    dp[start][end] = max;
+    return max;
+  }
+
   /**
    * 思路：采用逆序的dp，dp[i][j]表示戳i到j的气球，但是不包含i 和j
    * 那么就可以创造一个左边界为1，右边界为1的数组
@@ -12,7 +42,7 @@ public class Solution {
    * 即使dp公式有了以后，也需要控制循环次数，才能达到后面的用到前面的结果
    *
    */
-  public int maxCoins(int[] nums) {
+  public int maxCoins1(int[] nums) {
     int length = nums.length;
     int[][] dp = new int[length + 2][length + 2];
     int[] vals = new int[length + 2];
