93ce8168f67d7b5618ea2b9ca5b8c841cf864950
shuwei17 <Mhss@aspire121>
2021-02-03 09:31:17 +0800

    0203

--- src/main/java/shuwei/leetcode/cn/four/two/four/Solution.java
+++ src/main/java/shuwei/leetcode/cn/four/two/four/Solution.java
@@ -6,84 +6,104 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Solution {
-  public int characterReplacement(String s, int k) {
-    int ans = 0;
-    int length = s.length();
-    if (length <= 1) {
-      return s.length();
-    }
-    if (k == 0) {
-      int curr = 1;
-      char last = s.charAt(0);
-      for (int i = 1; i < s.length(); i++) {
-        if (s.charAt(i) == last) {
-          curr++;
-        } else {
-          ans = Math.max(ans, curr);
-          curr = 1;
-          last = s.charAt(i);
+    /**
+     * 这个能算出来了，但是需要15ms，效率比较差
+     * @param s
+     * @param k
+     * @return
+     */
+    public int characterReplacement(String s, int k) {
+        int ans = 0;
+        int length = s.length();
+        if (length <= 1 || k >= length) {
+            return s.length();
         }
-      }
-      return Math.max(ans, curr);
-    }
-    List<Integer>[] indexs = new List[26];
-    for (int i = 0; i < 26; i++) {
-      indexs[i] = new ArrayList<>();
-      indexs[i].add(0);
-    }
-    for (int i = 1; i < s.length(); i++) {
-      indexs[s.charAt(i) - 'A'].add(i);
-    }
-    Arrays.sort(indexs, Comparator.comparingInt(l -> l.size()));
-    char first = s.charAt(0);
-    for (int i = 25; i >= 0; i--) {
-      if (indexs[i].size() == 0 || indexs[i].size() + k <= ans) {
-        break;
-      }
-      if (i == first - 'A') {
-        ans = Math.max(ans, cal(indexs[i], k, length));
-      } else {
-        ans = Math.max(ans, cal(indexs[i], k - 1, length));
-      }
-    }
-    return ans;
-  }
-
-  private int cal(List<Integer> indexs, int k, int length) {
-    if (indexs.size() <= k || indexs.size() <= 1) {
-      return indexs.size() + k;
-    }
-    int size = indexs.size();
-    int ans = 0;
-    int result = 1;
-    int startIndex = 0;
-    int curr;
-    for (int i = 1; i < size; i++) {
-      curr = indexs.get(i - 1);
-      while (indexs.get(i) !=  curr + 1) {
         if (k == 0) {
-          ans = Math.max(ans, result);
-          startIndex++;
-          int diff = indexs.get(startIndex) - indexs.get(startIndex - 1);
-          while (startIndex < size && diff == 1) {
-            result--;
-            startIndex++;
-            diff = indexs.get(startIndex) - indexs.get(startIndex - 1);
-          }
-          result -= diff;
-          k += diff;
-        } else {
-          k--;
-          curr++;
-          result++;
+            int curr = 1;
+            char last = s.charAt(0);
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == last) {
+                    curr++;
+                } else {
+                    ans = Math.max(ans, curr);
+                    curr = 1;
+                    last = s.charAt(i);
+                }
+            }
+            return Math.max(ans, curr);
         }
-      }
-      result++;
+        List<Integer>[] indexs = new List[26];
+        for (int i = 0; i < 26; i++) {
+            indexs[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < s.length(); i++) {
+            indexs[s.charAt(i) - 'A'].add(i);
+        }
+        Arrays.sort(indexs, Comparator.comparingInt(l -> l.size()));
+        char first = s.charAt(0);
+        for (int i = 25; i >= 0; i--) {
+            if (indexs[i].size() == 0 || indexs[i].size() + k <= ans) {
+                break;
+            }
+            ans = Math.max(ans, cal(indexs[i], k, length));
+        }
+        return ans;
     }
-    if (length == indexs.get(size - 1) + 1) {
-      return Math.max(ans, result);
-    } else {
-      return Math.max(ans, result + (k <= length - indexs.get(size - 1) ? k : length - indexs.get(size - 1)));
+
+    private int cal(List<Integer> indexs, int k, int length) {
+        int size = indexs.size();
+        int start = 0, curr = 0, result = 0;
+        int remain = k;
+        while (curr < size - 1) {
+            int next = curr + 1;
+            int diff = indexs.get(next) - indexs.get(curr) - 1;
+            if (diff == 0) {
+                curr++;
+            } else {
+                // 是否有足够的remain让他们连起来
+                if (remain >= diff) {
+                    remain = remain - diff;
+                    curr = next;
+                } else {
+                    result = Math.max(result, indexs.get(curr) - indexs.get(start) + 1 + remain);
+                    if (start == curr) {
+                        start++;
+                        curr++;
+                        remain = k;
+                    } else {
+                        while (indexs.get(start + 1) == indexs.get(start) + 1) {
+                            start++;
+                        }
+                        if (start == curr) {
+                            remain = k;
+                        } else {
+                            start++;
+                            if (start < curr) {
+                                remain = remain + indexs.get(start) - indexs.get(start - 1) - 1;
+                            } else {
+                                remain = k;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 处理多余k的情况
+        if (remain > 0) {
+            int tail = length - indexs.get(size - 1) - 1;
+            if (tail >= remain) {
+                return Math.max(result, indexs.get(size - 1) - indexs.get(start) + 1 + remain);
+            } else {
+                remain = remain - tail;
+            }
+            int head = indexs.get(0);
+            if (head >= remain) {
+                return Math.max(result, indexs.get(size - 1) - indexs.get(start) + 1 + tail + remain);
+            } else {
+                return Math.max(result, indexs.get(size - 1) - indexs.get(start) + 1 + tail + head);
+            }
+        } else {
+            return Math.max(result, indexs.get(size - 1) - indexs.get(start) + 1);
+        }
     }
-  }
 }
--- src/test/java/shuwei/leetcode/cn/four/two/four/SolutionTest.java
+++ src/test/java/shuwei/leetcode/cn/four/two/four/SolutionTest.java
@@ -8,11 +8,12 @@ public class SolutionTest {
   @Test
   public void test() {
     Solution s = new Solution();
+    Assert.assertEquals(10, s.characterReplacement("KJRGKSKKOKLPADMAGODEDNEBMJMKGAPNLSAKADRLHHDRMJTMFBSIQGHENKABISHQNRFJKEPMFIPNDNEOBRJEHFLIHMDLMCIHLHQN", 5));
     Assert.assertEquals(7, s.characterReplacement("KRSCDCSONAJNHLBMDQGIFCPEKPOHQIHLTDIQGEKLRLCQNBOHNDQGHJPNDQPERNFSSSRDEQLFPCCCARFMDLHADJADAGNNSBNCJQOF", 4));
+    Assert.assertEquals(4, s.characterReplacement("AABABBA", 1));
     Assert.assertEquals(4, s.characterReplacement("ABBB", 2));
     Assert.assertEquals(5, s.characterReplacement("BAAAB", 2));
     Assert.assertEquals(2, s.characterReplacement("AABA", 0));
-    Assert.assertEquals(4, s.characterReplacement("AABABBA", 1));
     Assert.assertEquals(4, s.characterReplacement("ABAB", 2));
   }
 }
