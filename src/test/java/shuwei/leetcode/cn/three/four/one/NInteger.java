ae9c3b90f44d0974c8a3361a9ea5fa510c20c873
jimmyc2006 <shuwei@163.com>
2021-03-23 22:27:05 +0800

    0323

---
+++ src/test/java/shuwei/leetcode/cn/three/four/one/NInteger.java
@@ -0,0 +1,21 @@
+package shuwei.leetcode.cn.three.four.one;
+
+import java.util.List;
+
+public class NInteger implements NestedInteger {
+
+  @Override
+  public boolean isInteger() {
+    return false;
+  }
+
+  @Override
+  public Integer getInteger() {
+    return null;
+  }
+
+  @Override
+  public List<NestedInteger> getList() {
+    return null;
+  }
+}
