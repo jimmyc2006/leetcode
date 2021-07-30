8c69d749a15d1d20726fccdab1ef739c6db12a11
shuwei17 <Mhss@aspire222>
2021-07-30 09:21:38 +0800

    借助单调递增子序列的困难问题的解答

--- src/main/java/shuwei/leetcode/cn/one/seven/one/three/Solution.java
+++ src/main/java/shuwei/leetcode/cn/one/seven/one/three/Solution.java
@@ -1,5 +1,10 @@
 package shuwei.leetcode.cn.one.seven.one.three;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -8,6 +13,65 @@ public class Solution {
         System.out.println(solution.minOperations(new int[]{11, 16, 20, 1, 2, 13, 7, 6, 12, 3}, new int[]{11, 13, 3, 7, 7, 1, 10, 12, 14, 1})); //6
         System.out.println(solution.minOperations(new int[]{5, 1, 3}, new int[]{9, 4, 2, 3, 4})); // 2
         System.out.println(solution.minOperations(new int[]{6, 4, 8, 1, 3, 2}, new int[]{4, 7, 6, 2, 3, 8, 6, 1})); //3
+    }
+
+    /**
+     * 2021-07-30 review
+     * 去掉没有用的元素
+     * 将数值转化为下标
+     * 求最长的单调递增子序列
+     * @param target
+     * @param arr
+     * @return
+     */
+    public int minOperations(int[] target, int[] arr) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < target.length; i++) {
+            indexMap.put(target[i], i);
+        }
+        List<Integer> clears = new ArrayList<>();
+        for (int i : arr) {
+            Integer index = indexMap.get(i);
+            if (index != null) {
+                clears.add(index);
+            }
+        }
+        if (clears.isEmpty()) {
+            return target.length;
+        }
+        int[] dealedArr = clears.stream().mapToInt(i -> i).toArray();
+        // 最长单调递增子序列
+        int i = largeSubBigger(dealedArr);
+        return target.length - i;
+    }
+
+    private int largeSubBigger(int[] arr) {
+        int[] d = new int[arr.length + 1];
+        d[1] = arr[0];
+        int currIndex = 2;
+        for (int i = 1; i < arr.length; i++) {
+            int index = findBiggerIndex(d, 1, currIndex, arr[i]);
+            if (index == currIndex) {
+                d[currIndex++] = arr[i];
+            } else {
+                d[index] = arr[i];
+            }
+        }
+        return currIndex - 1;
+    }
+
+    private int findBiggerIndex(int[] arr, int start, int end, int val) {
+        if (start == end) {
+            return end;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] < val) {
+            return findBiggerIndex(arr, mid + 1, end, val);
+        } else if (arr[mid] == val) {
+            return mid;
+        } else {
+            return findBiggerIndex(arr, start, mid, val);
+        }
     }
 
     /**
@@ -32,7 +96,7 @@ public class Solution {
      * @param arr
      * @return
      */
-    public int minOperations(int[] target, int[] arr) {
+    public int minOperations_20210730(int[] target, int[] arr) {
         int[] count = new int[arr.length];
         int currMax = 0;
         for (int i = target.length - 1; i >= 0; i--) {
